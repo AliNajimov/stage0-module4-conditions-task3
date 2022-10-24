917d410f5a86d65f71ec01cfe8d8ad5af9dc8849
Muhammad Najimov <114020956+AliNajimov@users.noreply.github.com>
2022-10-24 15:41:47 +0500

    Update AliquotNumbers.java

--- src/main/java/school/mjc/stage0/conditions/task3/AliquotNumbers.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AliquotNumbers.java
@@ -2,10 +2,16 @@ package school.mjc.stage0.conditions.task3;
 
 public class AliquotNumbers {
     public void isFirstAliquot(int first, int second) {
-        if (first==5&&second==25){
+        int sum=0;
+        for(int i=1; i<=first/2; i++){
+            if(first%i==0){
+                sum+=i;
+            }
+        }
+        if (sum==second){
             System.out.println("Aliquot");
         }else {
-            System.out.println("Non Aliquot");
+            System.out.println("Not aliquot");
         }
     }
 }
