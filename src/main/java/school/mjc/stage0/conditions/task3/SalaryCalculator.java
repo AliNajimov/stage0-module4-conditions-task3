bb059e3578debf8ac3399334303c57d637b18df0
Muhammad Najimov <114020956+AliNajimov@users.noreply.github.com>
2022-10-24 17:57:06 +0500

    Update SalaryCalculator.java

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -5,13 +5,13 @@ public class SalaryCalculator {
         double temp=0;
         if ( salary <= 10000 && salary > 0) {
             temp = (salary*15)/100.0;
-            System.out.println(temp);
+            System.out.println(salary-temp);
         } else if ( salary > 1000 && salary <= 20000) {
             temp = (salary*18)/100.0;
-            System.out.println(temp);
+            System.out.println(salary-temp);
         } else if ( salary > 20000 ) {
             temp = (salary*20)/100.0;
-            System.out.println(temp);   
+            System.out.println(salary-temp);   
         } else if (salary < 0 ) {
             System.out.println("wrong input!");
         } 
