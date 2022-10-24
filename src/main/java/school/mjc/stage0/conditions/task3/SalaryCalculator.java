9d69afe7addf32957ca014876a0d20e58a42b01b
Muhammad Najimov <114020956+AliNajimov@users.noreply.github.com>
2022-10-24 13:42:10 +0500

    Update SalaryCalculator.java

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,7 +2,7 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
-        if ( salary <= 10000 ) {
+        if ( salary <= 10000 && salary > 0) {
             System.out.println(salary*1.5);
         } else if ( salary > 1000 && salary <= 20000) {
             System.out.println(salary*1.8);
